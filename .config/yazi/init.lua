b208cb54b23a5619ddce7768e3c349f0799c7f5e
davicro <david.rollonla@gmail.com>
2025-07-29 15:30:46 +0200

    Added yazi config

---
+++ .config/yazi/init.lua
@@ -0,0 +1,5 @@
+Status:children_add(function()
+    local command = "df -kh .|awk '!/^Filesystem/{printf \" %s FREE \", $(NF-2)}'"
+    local info = ui.Span(io.popen(command):read('*a')):fg("green")
+    return info
+end, 1500, Header.RIGHT)
