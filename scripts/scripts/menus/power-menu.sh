42902d9fdb7c21a9cf7b87320340c9ce21a843ac
davicro <david.rollonla@gmail.com>
2025-08-01 11:50:01 +0200

    Added Log out option to the power menu

--- scripts/scripts/menus/power-menu.sh
+++ scripts/scripts/menus/power-menu.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 ANCHOR=${1:-center}
-SELECTION="$(printf "1 - Shutdown\n2 - Reboot\n3 - Reboot to UEFI\n4 - Suspend" | \
-fuzzel --dmenu -l 4 -a $ANCHOR --keyboard-focus on-demand)"
+SELECTION="$(printf "1 - Shutdown\n2 - Reboot\n3 - Reboot to UEFI\n4 - Suspend\n5 - Log out" | \
+fuzzel --dmenu -l 5 -a $ANCHOR --keyboard-focus on-demand)"
 
 case $SELECTION in
 	*"Lock")
-		swaylock;;
+		hyprlock;;
 	*"Suspend")
 		systemctl suspend;;
 	*"Log out")
-		swaymsg exit;;
+		loginctl terminate-user "";;
 	*"Reboot")
 		systemctl reboot;;
 	*"Reboot to UEFI")
 		systemctl reboot --firmware-setup;;
-	*"Hard reboot")
-		pkexec "echo b > /proc/sysrq-trigger";;
 	*"Shutdown")
 		systemctl poweroff;;
 esac
