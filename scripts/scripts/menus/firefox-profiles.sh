e2492ce6e345f725676a5539154d07256c225a28
davicro <david.rollonla@gmail.com>
2025-08-05 23:00:55 +0200

    Improved firefox profile menu

--- scripts/scripts/menus/firefox-profiles.sh
+++ scripts/scripts/menus/firefox-profiles.sh
@@ -4,12 +4,14 @@ PROFILE_PATHS="$(cat $(xdg-user-dir DOCUMENTS)/.firefox-profiles)"
 
 [ -z "$PROFILE_PATHS" ] && exit 1;
 
-LINES="$(echo "$PROFILE_PATHS" | wc -l)"
-SELECTION="$(echo "$PROFILE_PATHS" | awk -F/ '{print $NF}' |\
+OPTIONS="$(echo "$PROFILE_PATHS" | grep -v "#\
+" | awk -F/ '{print $NF}')"
+LINES="$(echo "$OPTIONS" | wc -l)"
+SELECTION="$(echo "$OPTIONS" |\
 fuzzel --dmenu -l $LINES)"
 
 [ -z "$SELECTION" ] && exit 1;
 
 FULL_PATH="$(echo "$PROFILE_PATHS" | grep "$SELECTION")"
 
-uwsm app -- firefox --profile "$FULL_PATH"
+uwsm app -- firefox --profile "${FULL_PATH//#}"
