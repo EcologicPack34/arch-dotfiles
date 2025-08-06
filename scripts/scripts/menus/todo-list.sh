9a87fb77bd65c512a53cc368e0bf5a5cbc7b7757
davicro <david.rollonla@gmail.com>
2025-08-06 15:12:10 +0200

    Added todo list for fuzzel

---
+++ scripts/scripts/menus/todo-list.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TODO_FILE="$(xdg-user-dir DOCUMENTS)/todo.txt"
+
+[ -f "$TODO_FILE" ] || touch "$TODO_FILE";
+
+ITEMS="$(cat "$TODO_FILE")"
+COUNT="$(echo "$ITEMS" | wc -l)"
+
+[ -z "$ITEMS" ] && COUNT=0;
+
+SELECTION="$(echo "$ITEMS" | fuzzel --dmenu -w "50" -l "$COUNT" -p "// ")"
+
+[ -z "$SELECTION" ] && exit 1;
+
+# Fx matches the entire line for the exact string
+if grep -Fxq "$SELECTION" <<< "$ITEMS"; then
+	grep -Fxv "$SELECTION" <<< "$ITEMS" > "$TODO_FILE"
+else
+	echo "$SELECTION" >> "$TODO_FILE"
+fi
