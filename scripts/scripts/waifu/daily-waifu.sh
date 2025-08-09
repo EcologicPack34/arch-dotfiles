72a1a73aff4b55b87a01ec49778b58b3f930b625
davicro <david.rollonla@gmail.com>
2025-08-09 02:20:08 +0200

    Added waifu scripts :3
    Scripts that communicate with the waifu.pics api for investigative purposes

---
+++ scripts/scripts/waifu/daily-waifu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DAY_CODE="$(date +"%Y%m%d")"
+DAILY_WAIFU_DIR="/tmp/waifu"
+DAILY_WAIFU_FILE="$DAILY_WAIFU_DIR/$DAY_CODE"
+mkdir -p "$DAILY_WAIFU_DIR"
+
+# If the daily waifu is not found, download a new one
+if [ ! -f "$DAILY_WAIFU_FILE" ]; then
+	rm "$DAILY_WAIFU_DIR/*"
+	URL="$(~/scripts/waifu/waifu.sh "$@")"
+	curl "$URL" -s -o "$DAILY_WAIFU_FILE"
+fi
+
+echo "$DAILY_WAIFU_FILE"
