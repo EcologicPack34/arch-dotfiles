15c6cd9f384291a1a03ef7c3393663d31e573910
davicro <david.rollonla@gmail.com>
2025-08-12 14:52:31 +0200

    Added dynamic wallpaper switching through script

--- swww/.config/swww/swww.sh
+++ swww/.config/swww/swww.sh
@@ -1,6 +1,6 @@
-wallpaper_dir=$(xdg-user-dir WALLPAPERS)
+wallpaper_dir=~/.cache/wallpaper
 
 swww clear
-swww img $wallpaper_dir/frieren_grayscale.jpg --transition-type fade --transition-pos top-right --transition-duration 2 --transition-fps 60
+swww img $wallpaper_dir/current_wallpaper_grayscale --transition-type fade --transition-pos top-right --transition-duration 2 --transition-fps 60
 sleep 1
-swww img $wallpaper_dir/frieren.jpg  --transition-type grow  --transition-pos top-left --transition-duration 2 --transition-fps 60
+swww img $wallpaper_dir/current_wallpaper  --transition-type grow  --transition-pos top-left --transition-duration 2 --transition-fps 60
