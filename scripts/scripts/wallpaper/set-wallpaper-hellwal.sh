09d00a4c8caaed1ba805b4768ad1601fa6c4dc0c
davicro <david.rollonla@gmail.com>
2025-08-18 14:56:55 +0200

    Added random wallpaper choosing to set-wallpaper scripts
    If more than one argument is passed to either of the scripts, a random one will be picked. Right now it sucks because both of the scripts have some serious copy pasting, but it works for now.

--- scripts/scripts/wallpaper/set-wallpaper-hellwal.sh
+++ scripts/scripts/wallpaper/set-wallpaper-hellwal.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-ARG="$1"
 SET_WALLPAPER_SCRIPT="$HOME/scripts/wallpaper/set-wallpaper.sh"
 WALLPAPER_OUTPUT="$HOME/.cache/wallpaper"
 
-if [ -f  "$ARG" ]; then
-	$SET_WALLPAPER_SCRIPT "$@"
-	hellwal -i "$ARG" -m
-elif [ -d "$ARG" ]; then
+if [ $# -eq 1 ]; then
+	CHOSEN_WALLPAPER="$1"
+else
+	# Choose a random wallpaper if more than one argument passed
+	POSSIBLE_WALLPAPERS=("$@")
+	CHOSEN_WALLPAPER="${POSSIBLE_WALLPAPERS[RANDOM % $#]}"
+fi
+
+if [ -f  "$CHOSEN_WALLPAPER" ]; then
+	$SET_WALLPAPER_SCRIPT "$CHOSEN_WALLPAPER"
+	hellwal -i "$CHOSEN_WALLPAPER" -m
+elif [ -d "$CHOSEN_WALLPAPER" ]; then
 	echo "Working on it!"
 else
-	echo ""$ARG" does not exist" >&2
+	echo ""$CHOSEN_WALLPAPER" does not exist" >&2
 	exit 1
 fi
 
--- scripts/scripts/wallpaper/set-wallpaper.sh
+++ scripts/scripts/wallpaper/set-wallpaper.sh
@@ -4,8 +4,17 @@ WALLPAPER_OUTPUT=~/.cache/wallpaper/
 WALLPAPER_OUTPUT_FILE="$WALLPAPER_OUTPUT"/"current_wallpaper"
 mkdir "$WALLPAPER_OUTPUT" -p
 
-CHOSEN_WALLPAPER="$1"
-SET_WALLPAPER="${SET_WALLPAPER:-yes}" # In case the wallpaper's set elsewhere
+if [ $# -eq 1 ]; then
+	CHOSEN_WALLPAPER="$1"
+else
+	# Choose a random wallpaper if more than one argument passed
+	POSSIBLE_WALLPAPERS=("$@")
+	CHOSEN_WALLPAPER="${POSSIBLE_WALLPAPERS[RANDOM % $#]}"
+fi
+
+# In case the wallpaper's set elsewhere,
+# check the environment variable
+SET_WALLPAPER="${SET_WALLPAPER:-yes}" 
 
 [ -f "$CHOSEN_WALLPAPER" ] || {
 	echo "$CHOSEN_WALLPAPER does not exist" >&2
